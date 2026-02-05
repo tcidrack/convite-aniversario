a0acc7acf35bc52f36f68f8edd59fde9bee2d8e6
Thalison Cidrack <thalison.silva@mv.com.br>
2026-02-05 20:23:02 -0300

    fix router for vercel

--- rsvp-aniversario/frontend/src/App.jsx
+++ rsvp-aniversario/frontend/src/App.jsx
@@ -1,14 +1,17 @@
-import { Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Invite from "./pages/Invite"
 import Presentes from "./pages/Presentes"
 import Admin from "./pages/Admin"
+import "./index.css"
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Invite />} />
-      <Route path="/presentes" element={<Presentes />} />
-      <Route path="/admin" element={<Admin />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Invite />} />
+        <Route path="/presentes" element={<Presentes />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
