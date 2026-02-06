6ad65b1f925112dcdf10ac647d12a9922044ed73
Thalison Cidrack <thalison.silva@mv.com.br>
2026-02-05 21:18:41 -0300

    excluindo o vercel.js

--- rsvp-aniversario/frontend/src/App.jsx
+++ rsvp-aniversario/frontend/src/App.jsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import Invite from "./pages/Invite"
 import Presentes from "./pages/Presentes"
 import Admin from "./pages/Admin"
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Invite />} />
-        <Route path="/presentes" element={<Presentes />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Invite />} />
+      <Route path="/presentes" element={<Presentes />} />
+      <Route path="/admin" element={<Admin />} />
+    </Routes>
   )
 }
